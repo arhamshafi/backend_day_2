87c42758a7643cadc24990f54ab3ad94b7d6fb7a
arham <arhambutt2923@gmail.com>
2025-06-25 18:13:06 +0500

    minner change

--- frontend/src/Users.jsx
+++ frontend/src/Users.jsx
@@ -18,9 +18,9 @@ function Users() {
                     api && api.map((ele, idx) => {
                         return (
                             <div key={idx} className='w-[300px] h-[250px] cursor-pointer px-5 bg-green-600 rounded-xl mt-4 transition-all duration-150 ease-linear hover:scale-102 hover:bg-green-400'>
-                                <h1 className='text-center mt-5 font-bold text-white capitalize '> {ele.id} </h1>
+                                <h1 className='text-center mt-5 font-bold text-white capitalize '> id : {ele.id} </h1>
                                 <p className='text-white text-md '> Name : {ele.name} </p>
-                                <p className='text-white text-lg '> Email : {ele.email} </p>
+                                <p className='text-white text-md '> Email : {ele.email} </p>
                                 <p className='text-white text-lg '> Username : {ele.username} </p>
                                 <p className='text-white text-sm mt-5 '> Adress : {ele.address.street} , {ele.address.suite} , {ele.address.city} </p>
                                 <p className='text-white text-sm mt-5 font-bold '> Zipcode : {ele.address.zipcode} </p>
